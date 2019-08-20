13a7d2fab81a48ce4d26fcad90f50c9573740805
Bryan Liu <liubryan.developer@gmail.com>
2019-08-19 23:56:22 -0400

    changed to csv file for data instead of .txt

--- interface.c
+++ interface.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "interface.h"
 
 void print_list(FILE *fp)
 {
-    if ((fp = fopen("reminders.txt", "r")) == NULL)
-    {
-        fprintf(stderr, "Cannot open list\n");
-        //need any termination? break? exit? return?
-    }
-    else
-    {
-        printf("\n=========================\n");
-        char c;
-        while ((c = getc(fp)) != EOF)
-        {
-            putchar(c);
-        }
-        fclose(fp);
-        printf("\n=========================\n");
-    }
+    printf("\n==========================================\n");
+    system("(head -n 2 test.csv && tail -n+3 test.csv | sort -k1) | column -s, -t  | cat");
+    printf("==========================================\n");
 }
 
 void add_to_list(FILE *fp)
 {
-    if ((fp = fopen("reminders.txt", "a")) == NULL)
+    if ((fp = fopen("reminders.txt", "w")) == NULL)
     {
         fprintf(stderr, "Cannot open list\n");
     }
     else
     {
+        char class[100];
         char name[100];
         char date[12];
         char time[9];
         getchar();
+
+        printf("Enter class:");
+        fgets(class, 100, stdin);
+        class[strlen(class) - 1] = '\0';
 
         printf("Enter assignment name:");
         fgets(name, 100, stdin);
