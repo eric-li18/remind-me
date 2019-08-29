b742a086c3635fc0ed36f8c50c697712c80f15db
Bryan Liu <liubryan.developer@gmail.com>
2019-08-28 20:54:59 -0400

    fixed view function

--- interface.c
+++ interface.c
@@ -7,8 +7,10 @@
 
 void print_list(FILE *fp)
 {
+    char print_command[100];
+    sprintf(print_command, "(head -n 2 %s && tail -n+3 %s | sort -k1) | column -s, -t  | cat", FILE_NAME, FILE_NAME);
     printf("\n===========================================\n");
-    system("(head -n 2 reminders.csv && tail -n+3 reminders.csv | sort -k1) | column -s, -t  | cat");
+    system(print_command);
     printf("===========================================\n");
 }
 
@@ -50,13 +52,13 @@ void delete_from_list(FILE *fp)
 {
     char class_query[10];
     printf("Enter the course you want to delete from: ");
-    fgets(class_query,10,stdin);
-    class_query[strlen(class_query)-1] = '\0';
+    fgets(class_query, 10, stdin);
+    class_query[strlen(class_query) - 1] = '\0';
 
     char name_query[100];
     printf("Enter the assignment name: ");
-    fgets(name_query,100,stdin);
-    name_query[strlen(name_query)-1] = '\0';
+    fgets(name_query, 100, stdin);
+    name_query[strlen(name_query) - 1] = '\0';
 
     //char name[100];
     //char class[100];
@@ -65,10 +67,13 @@ void delete_from_list(FILE *fp)
     char row[1024];
     char *class_token;
     char *name_token;
-    while(fgets(row,1024,fp)){
-        while((class_token = strtok(row,",")) != NULL){
+    while (fgets(row, 1024, fp))
+    {
+        while ((class_token = strtok(row, ",")) != NULL)
+        {
             name_token = strtok(row, ",");
-            if(strcmp(class_token,class_query) && (name_token,name_query)){
+            if (strcmp(class_token, class_query) && (name_token, name_query))
+            {
                 //TODO
             }
         }
