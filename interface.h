e0ebc06e06041586dd9c5f8e24094afaa4bca660
Bryan Liu <liubryan.developer@gmail.com>
2019-08-29 15:04:36 -0400

    minor fix

--- interface.h
+++ interface.h
@@ -5,11 +5,11 @@
 
 void print_list(FILE *fp);
 
-char *trim_spaces(char *str);
-
 int check_valid_date(char date[11]);
 
 int check_valid_time(char time[100]);
+
+void add_to_list(FILE *fp);
 
 void delete_from_list();
 
