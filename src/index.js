0cf0b34deefe78cd4f7f206bcc308a9625af177a
Eric <erickh.li@mail.utoronto.ca>
2020-07-08 22:17:00 -0400

    added basic text for chatbot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = async function App(context) {
     if (context.event.isText) {
-        await context.sendText('Welcome to remind-me!');
+        await context.sendText('Hey there! I\'m the remind-me bot! \n\n I can help you schedule dates and remind you about upcoming important dates!');
     }
 };
